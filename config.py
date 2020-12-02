ff66ce0f73c22e9001d85a45b843a2b53564c60f
toremick <47187504+toremick@users.noreply.github.com>
2020-12-02 11:53:13 +0100

    Update config.py
    added credentials to config

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 from sys import platform
 from main.mqtt_as import config
-
+# mqtt settings
 config['server'] = '192.168.2.30'  # Change to suit
+
+# uncomment next two lines and set credentials if your mqtt broker uses authentication.
+# config['user'] = 'mqtt-username'
+# config['password'] = 'mqtt-password'
 
 # Set up your wifi here
 config['ssid'] = 'your-ssid'
