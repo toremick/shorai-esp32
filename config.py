7d417ef0225eca74dda319625bf4eeb17d010d0b
toremick <47187504+toremick@users.noreply.github.com>
2021-04-29 14:07:14 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ from sys import platform
 from main.mqtt_as import config
 # mqtt settings
 config['server'] = '192.168.2.30'  # Change to suit
+config['maintopic'] = 'heatpump'
 
 # uncomment next two lines and set credentials if your mqtt broker uses authentication.
 # config['user'] = 'mqtt-username'
