5efc7a1553be03bfbecc2931cf144109ac54bd2f
toremick <47187504+toremick@users.noreply.github.com>
2021-06-25 09:54:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+from main.ota_updater import OTAUpdater
+ssid = 'YOUR-SSID'
+password = 'YOUR-WIFI-PASSWORD'
+
+def download_and_install_update_if_available():
+    o = OTAUpdater('https://github.com/toremick/toshiba-hp-esp32')
+    o.check_for_update_to_install_during_next_reboot(ssid, password)
+    o.download_and_install_update_if_available(ssid, password)
+     
 def start():
     from main import inithp
     inithp.start_handshake()
     from main import heatpump
+    
+    
+
+def boot():
+    download_and_install_update_if_available()
+    start()
 
 
-start()
+boot()
