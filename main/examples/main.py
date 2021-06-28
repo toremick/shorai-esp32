d7445bfceae8e72ac2292080b65f281f4fd70cdd
toremick <47187504+toremick@users.noreply.github.com>
2021-06-28 19:40:23 +0200

    Update main.py

--- main/examples/main.py
+++ main/examples/main.py
@@ -1,9 +1,27 @@
 from main.ota_updater import OTAUpdater
+
 ssid = 'YOUR-SSID'
 password = 'YOUR-WIFI-PASSWORD'
+
+
+# MQTT settings
+config['server'] = '192.168.2.30'  # Change to suit
+config['maintopic'] = 'heatpump'
+# uncomment next two lines and set credentials if your mqtt broker uses authentication.
+# config['user'] = 'mqtt-username'
+# config['password'] = 'mqtt-password'
+
+# repo
 # REPLACE THE REPO URL WITH YOUR OWN!!
 # OR ELSE YOU WILL RECIEVE UPDATES FROM THIS REPO AT A REBOOT
 your_repo = 'https://github.com/toremick/shorai-esp32'
+
+
+
+config['ssid'] = ssid
+config['wifi_pw'] = password
+
+
 
 def download_and_install_update_if_available():
     o = OTAUpdater(your_repo)
