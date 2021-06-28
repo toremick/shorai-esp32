5c9f776479dc014fc763091d82e72764debd819e
toremick <47187504+toremick@users.noreply.github.com>
2021-06-28 19:54:38 +0200

    Update main.py

--- main/examples/main.py
+++ main/examples/main.py
@@ -1,43 +1,18 @@
 from main.ota_updater import OTAUpdater
-
-ssid = 'YOUR-SSID'
-password = 'YOUR-WIFI-PASSWORD'
-
-
-# MQTT settings
-config['server'] = '192.168.2.30'  # Change to suit
-config['maintopic'] = 'heatpump'
-# uncomment next two lines and set credentials if your mqtt broker uses authentication.
-# config['user'] = 'mqtt-username'
-# config['password'] = 'mqtt-password'
-
-# repo
-# REPLACE THE REPO URL WITH YOUR OWN!!
-# OR ELSE YOU WILL RECIEVE UPDATES FROM THIS REPO AT A REBOOT
-your_repo = 'https://github.com/toremick/shorai-esp32'
-
-
-
-config['ssid'] = ssid
-config['wifi_pw'] = password
-
-
+from config import config
 
 def download_and_install_update_if_available():
-    o = OTAUpdater(your_repo)
-    o.check_for_update_to_install_during_next_reboot(ssid, password)
-    o.download_and_install_update_if_available(ssid, password)
+    o = OTAUpdater(config['your_repo'])
+    o.check_for_update_to_install_during_next_reboot(config['ssid'] , config['wifi_pw'])
+    o.download_and_install_update_if_available(config['ssid'] , config['wifi_pw'])
      
 def start():
     from main import inithp
     inithp.start_handshake()
     from main import heatpump
-    
-    
 
 def boot():
     download_and_install_update_if_available()
     start()
-
 
 boot()
